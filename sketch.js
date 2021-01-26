9174f84ef902266fbd366a2e1e976d4730fb9051
parinith-2008 <71936428+parinith-2008@users.noreply.github.com>
2021-01-26 21:15:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+var gameState = "onSling"
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ground;
+var box1;
+var backgroundImg;
+var ball, slingshot;
+
+
+function preload() {
+  //preload the images here                                                           
+
+    backgroundImg = loadImage("sprites/GamingBackground.png");
+  }
+
+function setup(){
+    var canvas = createCanvas(3000,800);
+    engine = Engine.create();
+    world = engine.world;
+    
+
+    ground = new Ground(250,450,1200,20);
+    
+    box1 = new Box(700,100,70,70);
+    box2 = new Box(700,100,70,70);
+    box3 = new Box(700,100,70,70);
+    box4 = new Box(700,100,70,70);
+    box5 = new Box(700,100,70,70);
+   
+    box6 = new Box(700,100,70,70);
+    box7 = new Box(600,100,70,70);
+    box8 = new Box(600,100,70,70);
+    box9 = new Box(600,100,70,70);
+    box10 = new Box(600,100,70,70);
+
+    box11 = new Box(600,100,70,70);
+    box12= new Box(600,100,70,70);
+    box13 = new Box(500,100,70,70);
+    box14 = new Box(500,100,70,70);
+    box15 = new Box(500,100,70,70);
+   
+    box16 = new Box(500,100,70,70);
+    box17 = new Box(500,100,70,70);
+    box18 = new Box(500,100,70,70);
+    box19= new Box(500,100,70,70);
+    box20 = new Box(500,100,70,70);
+
+   ball = new Ball(400,200,80,80);
+    slingshot = new SlingShot (ball.body,{x:300, y:50});
+    slingshot.visible = false
+}
+
+function draw(){
+   
+    background(backgroundImg);
+    
+
+    Engine.update(engine);
+    //strokeWeight(4);
+
+    ground.display();
+
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    box15.display();
+    
+    box16.display();
+    box17.display();
+    box18.display();
+    box19.display();
+    box20.display();
+
+
+
+   ball.display();
+   
+    
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    if (gameState!=="launched") {
+        Matter.Body.setPosition(ball.body, {x: mouseX , y: mouseY});
+
+    }
+}
+
+
+
+
